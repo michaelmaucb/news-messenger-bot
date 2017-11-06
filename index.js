c8fe4ff1d9ea73fc42b28a534357f79cf88503e6
DChoi <DChoi@airbears2-10-142-38-113.airbears2.1918.berkeley.edu>
2017-11-06 12:03:02 -0800

    progress

--- index.js
+++ index.js
@@ -23,71 +23,71 @@ const
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-/*
+
 app.post('/webhook', (req, res) => {  
-  console.log("hello world")
+  console.log("hello cs160")
 
   // Parse the request body from the POST
-  // let body = req.body;
+  let body = req.body;
 
-  // // Check the webhook event is from a Page subscription
-  // if (body.object === 'page') {
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
 
-  //   // Iterate over each entry - there may be multiple if batched
-  //   body.entry.forEach(function(entry) {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
-  //     // Gets the body of the webhook event
-  //     let webhook_event = entry.messaging[0];
-  //     console.log(webhook_event);
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
 
-  //     // Get the sender PSID
-  //     let sender_psid = webhook_event.sender.id;
-  //     console.log('Sender PSID: ' + sender_psid);
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
 
-  //   });
+    });
 
-  //   // Return a '200 OK' response to all events
-  //   res.status(200).send('EVENT_RECEIVED');
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
 
-  // } else {
-  //   // Return a '404 Not Found' if event is not from a page subscription
-  //   res.sendStatus(404);
-  // }
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 
 });
-*/
+
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook/', (req, res) => {
-  res.send('hello you butt')
+  res.send('hello peeps')
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  // const VERIFY_TOKEN = "news";
+  const VERIFY_TOKEN = "news";
   
   // // Parse params from the webhook verification request
-  // let mode = req.query['hub.mode'];
-  // let token = req.query['hub.verify_token'];
-  // let challenge = req.query['hub.challenge'];
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
     
   // // Check if a token and mode were sent
-  // if (mode && token) {
+  if (mode && token) {
   
-  //   // Check the mode and token sent are correct
-  //   if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    // Check the mode and token sent are correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       
   //     // Respond with 200 OK and challenge token from the request
-  //     console.log('WEBHOOK_VERIFIED');
-  //     res.status(200).send(challenge);
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
     
-  //   } else {
-  //     // Responds with '403 Forbidden' if verify tokens do not match
-  //     res.sendStatus(403);      
-  //   }
-  // }
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
 });
 
 
 app.get('/', function(req, res) {
-  res.send('hello you butt')
+  res.send('hello world')
 })
 
 
