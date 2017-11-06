645c31bd444788d5e93f1e49fcd6d0feb1e74b1e
DChoi <DChoi@Davids-MacBook-Pro-5.local>
2017-11-05 23:00:33 -0800

    make it better

--- index.js
+++ index.js
@@ -1,76 +1,77 @@
+/*
+ * Starter Project for Messenger Platform Quick Start Tutorial
+ *
+ * Use this project as the starting point for following the 
+ * Messenger Platform quick start tutorial.
+ *
+ * https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
+ *
+ */
+
 'use strict';
 
 // Imports dependencies and set up http server
 const 
+  request = require('request'),
   express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+  body_parser = require('body-parser'),
+  app = express().use(body_parser.json()); // creates express http server
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-console.log('dklajflksjflasjflasjfklsaj');
-
-
-// Creates the endpoint for our webhook 
+// Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {  
+
+  // Parse the request body from the POST
   let body = req.body;
 
-  // Checks this is an event from a page subscription
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
-    console.log('david choi is sexier than michael ma');
+    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhookEvent = entry.messaging[0];
-      console.log(webhookEvent);
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      
     });
 
-    // Returns a '200 OK' response to all requests
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
+
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
 });
 
-app.get('/', function(req, res) {
-  res.send('hello you butt')
-})
-
-
-// Adds support for GET requests to our webhook
+// Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
-  console.log('David choi is sexy');
-
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "news";
-    
-  // Parse the query params
+  
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = "news";
+  
+  // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
     
-  // Checks if a token and mode is in the query string of the request
+  // Check if a token and mode were sent
   if (mode && token) {
   
-    // Checks the mode and token sent is correct
+    // Check the mode and token sent are correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       
-      // Responds with the challenge token from the request
+      // Respond with 200 OK and challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-
     
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
@@ -78,24 +79,6 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
-
-
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  
-}
-
-
 
 
 
