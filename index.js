318e76e9c295de894f69d8374c890b84af87e653
DChoi <DChoi@Davids-MacBook-Pro-5.local>
2017-11-05 22:02:41 -0800

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
-  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
