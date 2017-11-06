8c5539415555ae9d0faaa407c8616d058ae88671
DChoi <DChoi@Davids-MacBook-Pro-5.local>
2017-11-05 20:58:09 -0800

    test

--- index.js
+++ index.js
@@ -64,6 +64,21 @@ app.get('/webhook', (req, res) => {
   }
 });
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
+
 
 
 
