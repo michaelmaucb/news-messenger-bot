0101b1d72c743b892154f86dccb17960cbd9826f
DChoi <DChoi@airbears2-10-142-45-138.airbears2.1918.berkeley.edu>
2017-11-06 18:53:42 -0800

    make better

--- index.js
+++ index.js
@@ -25,13 +25,23 @@ app.post('/webhook', (req, res) => {
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-      console.log('this is cool')
 
       // Get the sender PSID
       // console.log(entry)
       // console.log(webhook_event.sender)
-      // let sender_psid = webhook_event.sender.id;
-      // console.log('Sender PSID: ' + sender_psid);
+
+      if (webhook_event.message) {
+        console.log(webhook_event)
+        if (webhook_event.message == 'TEST_MESSAGE') {
+          return
+        }
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
 
     });
 
@@ -72,6 +82,72 @@ app.get('/webhook/', (req, res) => {
   }
 });
 
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
+
+
+
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Checks if the message contains text
+  if (received_message.text) {
+    
+    // Creates the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+
+  } else if (received_message.attachments) {
+  
+    // Gets the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+  
+  } 
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
+
+
+function handlePostback(sender_psid, received_postback) {
+  let response;
+  
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
 
 
 
