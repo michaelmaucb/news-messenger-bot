30775930801b76ce02e0a3d3d6b02ff3f7d8fd77
jayiyer <jay.iyer@berkeley.edu>
2017-11-07 15:18:25 -0800

    get started and greeting text, maybe

--- index.js
+++ index.js
@@ -83,11 +83,122 @@ app.get('/webhook/', (req, res) => {
   }
 });
 
-
 app.get('/setup',function(req,res){
-
     setupGetStartedButton(res);
+    setupPersistentMenu(res);
+    setupGreetingText(res);
 });
+
+function setupGreetingText(res){
+  var messageData = {
+      "greeting":[
+          {
+          "locale":"default",
+          "text":"Greeting text for default local !"
+          }, {
+          "locale":"en_US",
+          "text":"Greeting text for en_US local !"
+          }
+      ]};
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+ PAGE_ACCESS_TOKEN,
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      form: messageData
+  },
+  function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          // Print out the response body
+          res.send(body);
+
+      } else { 
+          // TODO: Handle errors
+          res.send(body);
+      }
+  });
+
+}
+
+function setupPersistentMenu(res){
+  var messageData = 
+      {"persistent_menu":[
+          {
+          "locale":"default",
+          "composer_input_disabled":true,
+          "call_to_actions":[
+              {
+              "title":"Info",
+              "type":"nested",
+              "call_to_actions":[
+                  {
+                  "title":"Help",
+                  "type":"postback",
+                  "payload":"HELP_PAYLOAD"
+                  },
+                  {
+                  "title":"Contact Me",
+                  "type":"postback",
+                  "payload":"CONTACT_INFO_PAYLOAD"
+                  }
+              ]
+              },
+              {
+              "type":"web_url",
+              "title":"Visit website ",
+              "url":"http://www.techiediaries.com",
+              "webview_height_ratio":"full"
+              }
+          ]
+          },
+          {
+          "locale":"zh_CN",
+          "composer_input_disabled":false
+          }
+      ]};  
+  // Start the request
+  request({
+      url: "https://graph.facebook.com/v2.6/me/messenger_profile?access_token="+ PAGE_ACCESS_TOKEN,
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      form: messageData
+  },
+  function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          // Print out the response body
+          res.send(body);
+
+      } else { 
+          // TODO: Handle errors
+          res.send(body);
+      }
+  });
+
+}
+
+function setupGetStartedButton(res){
+  var messageData = {
+          "get_started":{
+              "payload":"getstarted"
+          }
+  };
+  // Start the request
+  request({
+      url: "https://graph.facebook.com/v2.6/me/messenger_profile?access_token="+ PAGE_ACCESS_TOKEN,
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      form: messageData
+  },
+  function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          // Print out the response body
+          res.send(body);
+
+      } else { 
+          // TODO: Handle errors
+          res.send(body);
+      }
+  });
+}
 
 function callSendAPI(sender_psid, response) {
   // Construct the message body
