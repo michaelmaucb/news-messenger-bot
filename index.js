e40553086aa65ea1a709e23df9712302ef8f06b6
DChoi <DChoi@airbears2-10-142-44-67.airbears2.1918.berkeley.edu>
2017-11-07 21:01:55 -0800

    fix progress

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/webhook', (req, res) => {
                     //present user with some greeting or call to action
                     var msg = "Hi ,I'm a Bot ,and I was created to help you easily .... "
                     console.log(msg)
-                    //sendMessage(event.sender.id,msg);      
+                    handleMessage(event.sender.id,msg);      
             } 
           }
         });
