b73f13474a3e03ff7d427b48bc3613d87798207b
DChoi <DChoi@airbears2-10-142-44-67.airbears2.1918.berkeley.edu>
2017-11-07 20:52:29 -0800

    fix progress

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/webhook', (req, res) => {
         console.log(entry)
         console.log("david choi")
         var msg = "Hi ,I'm a Bot ,and I was created to help you easily .... "
-        handleMessage(entry.sender.id, msg)
+        handleMessage(entry.sender[0].id, msg)
 
       }
 
