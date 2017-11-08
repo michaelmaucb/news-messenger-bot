25d5d550ef78903474c955103064b1a1dc9333f6
DChoi <DChoi@airbears2-10-142-44-67.airbears2.1918.berkeley.edu>
2017-11-07 20:30:15 -0800

    fix progress

--- index.js
+++ index.js
@@ -24,47 +24,38 @@ app.post('/webhook', (req, res) => {
     body.entry.forEach(function(entry) {
 
       // Gets the body of the webhook event
-      // let webhook_event = entry.messaging[0];
-      // console.log(webhook_event);
+      console.log(entry)
+      console.log("____________________")
+      console.log(entry.messaging)
+      console.log("____________________")
+      console.log(entry.messaging[0])
 
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
 
+      // Get the sender PSID
+      // console.log(entry)
+      // console.log(webhook_event.sender)
 
-      // if (webhook_event.message) {
-      //   console.log(webhook_event)
-      //   if (webhook_event.message == 'TEST_MESSAGE') {
-      //     return
-      //   }
-      //   let sender_psid = webhook_event.sender.id;
-      //   console.log('Sender PSID: ' + sender_psid);
+      if (webhook_event.message) {
+        console.log(webhook_event)
+        if (webhook_event.message == 'TEST_MESSAGE') {
+          return
+        }
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
 
-      //   handleMessage(sender_psid, webhook_event.message);        
-      // } else if (webhook_event.postback) {
-      //   handlePostback(sender_psid, webhook_event.postback);
-      // } else {
-      //   if(event.postback && event.postback.payload === USER_DEFINED_PAYLOAD ){
-      //           //present user with some greeting or call to action
-      //     var msg = "Hi ,I'm a News-Flash-Bot"
-      //     sendMessage(event.sender.id,msg);
-      //   } 
-      // }
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      } else {
+        if(event.postback && event.postback.payload === USER_DEFINED_PAYLOAD ){
+                //present user with some greeting or call to action
+          var msg = "Hi ,I'm a News-Flash-Bot"
+          handleMessage(sender_psid,msg);
+        } 
+      }
 
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-          if (event.message) {
-          //receivedMessage(event);
-          } else {
-              // If the event is a postback and has a payload equals USER_DEFINED_PAYLOAD 
-          if(event.postback && event.postback.payload === USER_DEFINED_PAYLOAD )
-          {
-                  //present user with some greeting or call to action
-                  var msg = "Hi ,I'm a Bot ,and I was created to help you easily .... "
-                  handleMessage(event.sender.id,msg);      
-          }      
-
-          }
-      });
     });
 
     // Returns a '200 OK' response to all requests
