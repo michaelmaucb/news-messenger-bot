7889e2c60cd0f389b78948e738f340e17fc428be
DChoi <DChoi@airbears2-10-142-44-67.airbears2.1918.berkeley.edu>
2017-11-07 21:17:30 -0800

    fix progress

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.post('/webhook', (req, res) => {
                     //present user with some greeting or call to action
                     var msg = "Hi ,I'm a Bot ,and I was created to help you easily .... "
                     console.log(msg)
+                    let response;
                     response = {
                       "text": msg
                     }
