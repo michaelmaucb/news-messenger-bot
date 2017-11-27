cb1052e31d5395d6c83de6fdaaa607605dd8039d
Michael Ma <michaelma1210@gmail.com>
2017-11-27 12:55:18 -0800

    fix

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ function handleMessage(sender_psid, received_message) {
   console.log("REICEIVED MESSAGE:" + received_message)
   console.log("_________________________________")
 
-  if (received_message.text == 'news' || received_message.text == 'News') {
+  if (received_message.text == 'news') {
     console.log("goes through news")
 
     response = {
