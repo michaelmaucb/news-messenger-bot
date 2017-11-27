2969efb044c5ad08bcc61437f3b0833bf232777d
Michael Ma <michaelma1210@gmail.com>
2017-11-27 12:12:00 -0800

    f

--- index.js
+++ index.js
@@ -405,22 +405,13 @@ function handlePostback(sender_psid, received_postback) {
 
         "payload": {
           "template_type": "list",
-          "top_element_style": "compact",
+          // "top_element_style": "compact",
           "elements": [
             {
               "title": "Classic T-Shirt Collection",
               "subtitle": "See all our colors",
-              "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-              "buttons": [
-                {
-                  "title": "View",
-                  "type": "web_url",
-                  "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                  // "messenger_extensions": true,
-                  // "webview_height_ratio": "tall",
-                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                }
-              ]
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png"
+
             }
           ]
         }
